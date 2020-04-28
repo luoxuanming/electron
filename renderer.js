b9118bb78778c4e95a22705de9966c61e88082b9
luoxuanming <1316570222@qq.com>
2020-04-28 18:29:39 +0800

    3

--- renderer.js
+++ renderer.js
@@ -4,3 +4,59 @@
 // `nodeIntegration` is turned off. Use `preload.js` to
 // selectively enable features needed in the rendering
 // process.
+const electron = require("electron");
+const fs = require("fs");
+let oBtn = document.getElementById("btn");
+oBtn.onclick = function () {
+  console.log("getCPUUsage:", process.getCPUUsage())
+}
+
+// Files 对象实例
+const dragWrapper = document.getElementById("drag_test");
+dragWrapper.addEventListener("drop", (e) => {
+  e.preventDefault();
+  const files = e.dataTransfer.files;
+  if (files && files.length > 0) {
+    const path = files[0].path;
+    console.log("path:", path);
+    const content = fs.readFileSync(path);
+    console.log(content.toString())
+  }
+});
+
+dragWrapper.addEventListener("dragover", (e) => {
+  e.preventDefault();
+});
+
+//webview实例
+const wb = document.getElementById("wb");
+const loading = document.querySelector("#loading")
+wb.addEventListener("did-start-loading", () => {
+  loading.innerHTML = "loading..."
+});
+  wb.addEventListener("did-stop-loading", () => {
+    loading.innerHTML = "OK."
+    wb.insertCSS(`
+    #su {
+      background: red !important;
+    }
+`);
+  wb.executeJavaScript(`
+    setTimeout(()=>{
+      alert(document.getElementById('su').value);
+    },2000);
+  `);  
+  // wb.openDevTools();
+});
+
+
+
+
+function openNewWindow() {
+  window.open("https://www.baidu.com","baidu");
+}
+
+let oBtn2 = document.getElementById("btn2");
+oBtn2.onclick = function () {
+  openNewWindow()
+}
---
+++ webview_test/preload.js
@@ -0,0 +1,7 @@
+setTimeout(()=>{
+  // alert(document.querySelector('.index-logo-src').src);
+  // document.querySelector('#su').onclick=()=>{
+  //   alert("您点击了搜索按钮")
+  // }
+  
+},5000);
